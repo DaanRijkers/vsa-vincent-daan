91541d7a60f244128b412f90f7c7ed92499636e7
rijkers.daan@gmail.com <rijkers.daan@gmail.com@76514828-0ce1-decd-b7e2-70ea8b6a4066>
2014-05-30 20:11:00 +0000

    - Max/min scales

--- Triangulation Editor/src/editor/domain/Grid.java
+++ Triangulation Editor/src/editor/domain/Grid.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package editor.domain;
 
 import java.awt.BasicStroke;
@@ -16,46 +15,57 @@ import java.util.List;
  * @author Daan
  */
 public class Grid implements IDrawable {
-    
+
     private int spacing;
     private int screenWidth;
     private int screenHeight;
-    
-    
+
     public Grid() {
         this.spacing = 0;
         this.screenWidth = 0;
         this.screenHeight = 0;
     }
-    
+
     public Grid(int sp, int width, int height) {
         this.spacing = sp;
         this.screenWidth = width;
         this.screenHeight = height;
     }
-    
+
     @Override
     public void draw(Graphics2D g, double scale) {
         g.setColor(Color.LIGHT_GRAY);
-        
+
+        int counter = 0;
         // Vertical lines
-        for (double i = (spacing * scale); i < screenWidth; i += (spacing * scale)) {            
-            g.drawLine((int)i, 0, (int)i, screenHeight);
-            g.drawString(String.valueOf(((int)(i / scale))), (int)i - (g.getFont().getSize() / 2), g.getFont().getSize());
+        for (double i = (spacing * scale); i < screenWidth; i += (spacing * scale)) {
+            g.drawLine((int) i, 0, (int) i, screenHeight);
+
+            counter++;
+            if (scale > 0.5 || counter == 3) {
+                g.drawString(String.valueOf(((int) (i / scale))), (int) i - (g.getFont().getSize() / 2), g.getFont().getSize());
+                counter = 0;
+            }
         }
-        
+
+        counter = 0;
         // Horizontal lines
         for (double i = (spacing * scale); i < screenHeight; i += (spacing * scale)) {
-            g.drawLine(0, (int)i, screenWidth, (int)i);
-            g.drawString(String.valueOf((int)(i / scale)), 1, (int)i + (g.getFont().getSize() / 2) - 1);
+            g.drawLine(0, (int) i, screenWidth, (int) i);
+
+            counter++;
+            if (scale > 0.5 || counter == 3) {
+                g.drawString(String.valueOf((int) (i / scale)), 1, (int) i + (g.getFont().getSize() / 2) - 1);
+                counter = 0;
+            }
         }
     }
-    
+
     @Override
     public IDrawable checkSelection(int mouseX, int mouseY, boolean multiSelect) {
         return null;
-    }    
-    
+    }
+
     @Override
     public List<IDrawable> checkSelection(int mouseX, int mouseY, int width, int height, boolean multiSelect) {
         return null;
